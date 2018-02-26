f8c454f5711bc96ecf4494c68961d5783d1b97d4
Tom Homer <tomhomer@gmail.com>
2018-02-26 13:40:02 -0500

    Fixed commenting etc for denim templates functions file

--- templates/denim/functions.php
+++ templates/denim/functions.php
@@ -1,7 +1,7 @@
 <?php
 
-// Allows plugin to add extra css and javascript libraries for individual themes.
-// Theme settings will overwrite any plugin settimgs that are the same name/file
+// Allows plugin to add extra CSS and javascript libraries for individual themes.
+// Theme settings will overwrite any plugin settings that are the same name/file
 
 
 // this file can't be used on its own (keep this in as plugin template files could be located with theme) 
@@ -17,29 +17,18 @@ function forum_css_denim()
 {
     global $_CONF, $LANG_DIRECTION;
 
+    // No extra CSS files needed by plugin. Assume theme loads all required.
     return array();
 
+    // BELOW NOT NEEDED ANYMORE AS LOADED BY THEME NOW
+    // The only extra component that the plugin needs from uikit is the tooltip
     $direction = ($LANG_DIRECTION == 'rtl') ? '_rtl' : '';
-
     return array(
-        array(
-            'name'       => 'uikit',
-            'file'       => '/vendor/uikit/css' . $direction . '/uikit.gradient.min.css',
-            'attributes' => array('media' => 'all'),
-            'priority'   => 80
-        ),
-        
         array(
             'name'       => 'uikit-tooltip',
             'file'       => '/vendor/uikit/css' . $direction . '/components/tooltip.gradient.min.css',
             'attributes' => array('media' => 'all'),
             'priority'   => 70
-        ),        
-
-        array(
-            'name'       => 'main', // don't use the name 'theme' to control the priority
-            'file'       => '/layout/' . $_CONF['theme'] . '/css_' . $LANG_DIRECTION . '/style.css', // change '/style.css' during debugging
-            'attributes' => array('media' => 'all')
         )
     );
 }
@@ -49,14 +38,8 @@ function forum_css_denim()
  */
 function forum_js_libs_denim()
 {
+    // No extra JS libraries needed by plugin. Assume theme loads all required.
     return array();
-
-    return array(
-       array(
-            'library' => 'jquery',
-            'footer'  => false // Not requred, default = true
-        )
-    );
 }
 
 /**
@@ -66,26 +49,16 @@ function forum_js_files_denim()
 {
     global $_CONF;
 
+    // No extra JS libraries needed by plugin. Assume theme loads all required.
     return array();
 
+    // BELOW NOT NEEDED ANYMORE AS LOADED BY THEME NOW
+    // The only component that the plugin needs from uikit is the tooltip 
     return array(
-
-       array(
-            'file'      => '/vendor/uikit/js/uikit.js',
-            'footer'    => false, // Not requred, default = true
-            'priority'  => 100 // Not requred, default = 100
-        ),
-       
        array(
             'file'      => '/vendor/uikit/js/components/tooltip.js',
-            'footer'    => false, // Not requred, default = true
-            'priority'  => 110 // Not requred, default = 100
-        ),       
-
-       array(
-            'file'      => '/layout/' . $_CONF['theme'] . '/javascript/script.js',
-            'footer'    => true, // Not requred, default = true
-            'priority'  => 100 // Not requred, default = 100
+            'footer'    => false, // Not required, default = true
+            'priority'  => 110 // Not required, default = 100
         )
     );
 }
